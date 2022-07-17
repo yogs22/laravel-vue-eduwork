7fcfa2234c45d385e16b9d3a20eb29518cd069c6
unknown <yongkisurya99@gmail.com>
2022-07-17 23:29:37 +0700

    menambahkan tabel & relasi untuk task 2

--- yongki/apotek.sql
+++ yongki/apotek.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 13 Jul 2022 pada 14.44
+-- Waktu pembuatan: 17 Jul 2022 pada 18.22
 -- Versi server: 10.4.24-MariaDB
 -- Versi PHP: 8.1.6
 
@@ -20,6 +20,36 @@ SET time_zone = "+00:00";
 --
 -- Database: `apotek`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `faktur_supply`
+--
+
+CREATE TABLE `faktur_supply` (
+  `no` int(8) NOT NULL,
+  `tanggal` date NOT NULL,
+  `id_karyawan` int(8) NOT NULL,
+  `id_supplier` int(8) NOT NULL,
+  `id_obat` int(8) NOT NULL,
+  `jumlah_obat` int(8) DEFAULT NULL,
+  `harga` int(16) DEFAULT NULL,
+  `total_harga` int(16) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `karyawan`
+--
+
+CREATE TABLE `karyawan` (
+  `id` int(8) NOT NULL,
+  `nama` varchar(32) NOT NULL,
+  `alamat` varchar(64) NOT NULL,
+  `no_telepon` int(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -46,26 +76,51 @@ INSERT INTO `obat` (`id`, `nama_obat`, `pembuat_obat`, `tanggal_kadaluwarsa`) VA
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `transaksi`
+-- Struktur dari tabel `supplier`
 --
 
-CREATE TABLE `transaksi` (
-  `id_transaksi` int(11) DEFAULT NULL,
-  `nama_pasien` varchar(32) NOT NULL,
-  `id_obat` int(11) NOT NULL,
-  `jumlah_transaksi` int(11) DEFAULT NULL
+CREATE TABLE `supplier` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(32) NOT NULL,
+  `Alamat` varchar(64) NOT NULL,
+  `no_telepon` char(13) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
 --
--- Dumping data untuk tabel `transaksi`
+-- Struktur dari tabel `transaksi_penjualan`
 --
 
-INSERT INTO `transaksi` (`id_transaksi`, `nama_pasien`, `id_obat`, `jumlah_transaksi`) VALUES
-(NULL, 'Yongki', 3, 3);
+CREATE TABLE `transaksi_penjualan` (
+  `no` int(8) NOT NULL,
+  `tanggal` date NOT NULL,
+  `nama_ customer` varchar(32) NOT NULL,
+  `id_karyawan` int(8) NOT NULL,
+  `id_obat` int(8) NOT NULL,
+  `jumlah` int(8) NOT NULL,
+  `harga` int(16) NOT NULL,
+  `total_harga` int(16) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indeks untuk tabel `faktur_supply`
+--
+ALTER TABLE `faktur_supply`
+  ADD PRIMARY KEY (`no`),
+  ADD KEY `fk_supplier` (`id_supplier`),
+  ADD KEY `fk_karyawan` (`id_karyawan`),
+  ADD KEY `fk_obat` (`id_obat`);
+
+--
+-- Indeks untuk tabel `karyawan`
+--
+ALTER TABLE `karyawan`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indeks untuk tabel `obat`
@@ -74,14 +129,35 @@ ALTER TABLE `obat`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `transaksi`
+-- Indeks untuk tabel `supplier`
 --
-ALTER TABLE `transaksi`
-  ADD PRIMARY KEY (`id_obat`);
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `transaksi_penjualan`
+--
+ALTER TABLE `transaksi_penjualan`
+  ADD PRIMARY KEY (`no`),
+  ADD KEY `tanggal` (`tanggal`),
+  ADD KEY `id_karyawan` (`id_karyawan`),
+  ADD KEY `id_obat` (`id_obat`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
 --
+
+--
+-- AUTO_INCREMENT untuk tabel `faktur_supply`
+--
+ALTER TABLE `faktur_supply`
+  MODIFY `no` int(8) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `karyawan`
+--
+ALTER TABLE `karyawan`
+  MODIFY `id` int(8) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT untuk tabel `obat`
@@ -90,20 +166,35 @@ ALTER TABLE `obat`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `transaksi`
+-- AUTO_INCREMENT untuk tabel `supplier`
 --
-ALTER TABLE `transaksi`
-  MODIFY `id_obat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `supplier`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT untuk tabel `transaksi_penjualan`
+--
+ALTER TABLE `transaksi_penjualan`
+  MODIFY `no` int(8) NOT NULL AUTO_INCREMENT;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
 --
 
 --
--- Ketidakleluasaan untuk tabel `transaksi`
+-- Ketidakleluasaan untuk tabel `faktur_supply`
 --
-ALTER TABLE `transaksi`
-  ADD CONSTRAINT `fk_trasnsaksi` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`);
+ALTER TABLE `faktur_supply`
+  ADD CONSTRAINT `fk_karyawan` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id`),
+  ADD CONSTRAINT `fk_obat` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`),
+  ADD CONSTRAINT `fk_supplier` FOREIGN KEY (`id_supplier`) REFERENCES `supplier` (`id`);
+
+--
+-- Ketidakleluasaan untuk tabel `transaksi_penjualan`
+--
+ALTER TABLE `transaksi_penjualan`
+  ADD CONSTRAINT `transaksi_penjualan_ibfk_1` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`),
+  ADD CONSTRAINT `transaksi_penjualan_ibfk_2` FOREIGN KEY (`id_karyawan`) REFERENCES `karyawan` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
