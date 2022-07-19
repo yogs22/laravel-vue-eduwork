89d99ddf158c89e3df27b92cd97e3e5230a81562
FauziNurdiansyah <108964908+FauziNurdiansyah@users.noreply.github.com>
2022-07-19 21:35:47 +0700

    repost tugas Task 3

--- Fauzi/apotek_db.sql
+++ Fauzi/apotek_db.sql
@@ -1,14 +1,13 @@
--- phpMyAdmin SQL Dump 
--- version 4.8.0.1
+-- phpMyAdmin SQL Dump
+-- version 5.3.0-dev+20220715.346923e20a
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Jul 2022 pada 08.38
+-- Waktu pembuatan: 19 Jul 2022 pada 15.14
 -- Versi server: 10.1.32-MariaDB
 -- Versi PHP: 7.2.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
