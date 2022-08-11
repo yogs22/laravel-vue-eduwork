2d9d0317e93a7a466678d8a995902257977a9e52
thisnotme2205 <thisnotme.2205@gmail.com>
2022-08-11 07:26:50 +0700

    update task simple query sql

--- Rofli/apotek.sql
+++ Rofli/apotek.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 13 Jul 2022 pada 18.36
--- Versi server: 10.4.21-MariaDB
--- Versi PHP: 7.4.25
+-- Host: localhost
+-- Generation Time: Aug 11, 2022 at 01:48 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,45 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `jenis`
+-- Table structure for table `detail_transaksi`
+--
+
+CREATE TABLE `detail_transaksi` (
+  `id_transaksi` int(11) NOT NULL,
+  `id_obat` int(11) NOT NULL,
+  `jumlah` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `detail_transaksi`
+--
+
+INSERT INTO `detail_transaksi` (`id_transaksi`, `id_obat`, `jumlah`) VALUES
+(1, 4, 2),
+(1, 5, 5),
+(2, 7, 1),
+(2, 4, 2),
+(2, 6, 1),
+(3, 2, 1),
+(4, 6, 1),
+(4, 1, 1),
+(5, 2, 1),
+(5, 4, 3),
+(5, 8, 1),
+(6, 2, 2),
+(6, 10, 1),
+(6, 5, 2),
+(7, 5, 1),
+(7, 1, 1),
+(8, 6, 1),
+(9, 8, 2),
+(10, 5, 2),
+(10, 3, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jenis`
 --
 
 CREATE TABLE `jenis` (
@@ -33,76 +71,238 @@ CREATE TABLE `jenis` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `jenis`
+-- Dumping data for table `jenis`
 --
 
 INSERT INTO `jenis` (`id`, `nama`) VALUES
 (1, 'Luka Luar'),
-(2, 'Luka Dalam');
+(2, 'Luka Dalam'),
+(3, 'Obat Pusing'),
+(4, 'Obat Mual'),
+(5, 'Obat Mata'),
+(6, 'Obat Perut'),
+(7, 'Obat Nyeri'),
+(8, 'Obat Tetes'),
+(9, 'Vitamin'),
+(10, 'Sirup');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `obat`
+-- Table structure for table `obat`
 --
 
 CREATE TABLE `obat` (
   `id` int(11) NOT NULL,
   `nama` varchar(20) NOT NULL,
-  `id_jenis` int(11) NOT NULL
+  `id_jenis` int(11) NOT NULL,
+  `harga` int(6) NOT NULL,
+  `stok` int(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Dumping data untuk tabel `obat`
+-- Dumping data for table `obat`
 --
 
-INSERT INTO `obat` (`id`, `nama`, `id_jenis`) VALUES
-(1, 'Betadine', 1),
-(2, 'Calpanax', 1),
-(3, 'Paramex', 2);
+INSERT INTO `obat` (`id`, `nama`, `id_jenis`, `harga`, `stok`) VALUES
+(1, 'Betadine', 1, 16000, 20),
+(2, 'Calpanax', 1, 8000, 15),
+(3, 'Paramex', 2, 4000, 60),
+(4, 'Bodrex', 3, 3000, 5),
+(5, 'Bodrexin', 3, 2000, 0),
+(6, 'Diapet', 6, 8000, 10),
+(7, 'Antangin', 4, 7500, 8),
+(8, 'Antimo', 4, 4500, 25),
+(9, 'Fatigue', 9, 12000, 15),
+(10, 'Insto', 5, 25000, 4);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pegawai`
+--
+
+CREATE TABLE `pegawai` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jk` int(1) NOT NULL,
+  `hp` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pegawai`
+--
+
+INSERT INTO `pegawai` (`id`, `nama`, `jk`, `hp`) VALUES
+(1, 'Rofli', 1, '0812345678'),
+(2, 'Septi', 2, '087654321'),
+(3, 'Dika', 1, '0826672546'),
+(4, 'Gilang', 1, '082163623'),
+(5, 'Ilham', 1, '0875213'),
+(6, 'Ayu', 2, '012635123'),
+(7, 'Intan', 2, '012637526'),
+(8, 'Dimas', 1, '0267352'),
+(9, 'Putri', 2, '021637652'),
+(10, 'Yuni', 2, '0127632837');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembeli`
+--
+
+CREATE TABLE `pembeli` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(50) NOT NULL,
+  `jk` int(1) NOT NULL,
+  `hp` varchar(13) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pembeli`
+--
+
+INSERT INTO `pembeli` (`id`, `nama`, `jk`, `hp`) VALUES
+(1, 'Yadi', 1, '087890123'),
+(2, 'Septiadi', 1, '081235326'),
+(3, 'Farhan', 1, '023723832'),
+(4, 'Rayhan', 1, '012835283'),
+(5, 'Indah', 2, '0123523'),
+(6, 'Sindy', 2, '93621362'),
+(7, 'Upin', 1, '12386293'),
+(8, 'Ipin', 1, '2836329'),
+(9, 'Fizi', 1, '018236'),
+(10, 'Mail', 1, '1273652');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE `transaksi` (
+  `id` int(11) NOT NULL,
+  `id_pegawai` int(11) NOT NULL,
+  `id_pembeli` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id`, `id_pegawai`, `id_pembeli`) VALUES
+(1, 4, 5),
+(2, 2, 9),
+(3, 8, 8),
+(4, 4, 3),
+(5, 7, 8),
+(6, 5, 4),
+(7, 3, 2),
+(8, 4, 5),
+(9, 4, 9),
+(10, 6, 10);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `jenis`
+-- Indexes for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD KEY `id_transaksi` (`id_transaksi`),
+  ADD KEY `id_obat` (`id_obat`);
+
+--
+-- Indexes for table `jenis`
 --
 ALTER TABLE `jenis`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `obat`
+-- Indexes for table `obat`
 --
 ALTER TABLE `obat`
   ADD PRIMARY KEY (`id`),
   ADD KEY `id_jenis` (`id_jenis`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- Indexes for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_pegawai` (`id_pegawai`),
+  ADD KEY `id_pembeli` (`id_pembeli`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `jenis`
+-- AUTO_INCREMENT for table `jenis`
 --
 ALTER TABLE `jenis`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT untuk tabel `obat`
+-- AUTO_INCREMENT for table `obat`
 --
 ALTER TABLE `obat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- AUTO_INCREMENT for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `pembeli`
+--
+ALTER TABLE `pembeli`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `obat`
+-- Constraints for table `detail_transaksi`
+--
+ALTER TABLE `detail_transaksi`
+  ADD CONSTRAINT `detail_transaksi_ibfk_1` FOREIGN KEY (`id_transaksi`) REFERENCES `transaksi` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detail_transaksi_ibfk_2` FOREIGN KEY (`id_obat`) REFERENCES `obat` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `obat`
 --
 ALTER TABLE `obat`
   ADD CONSTRAINT `obat_ibfk_1` FOREIGN KEY (`id_jenis`) REFERENCES `jenis` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_pegawai`) REFERENCES `pegawai` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_pembeli`) REFERENCES `pembeli` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
