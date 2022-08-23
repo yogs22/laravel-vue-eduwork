64b320f3e78a3a2d1b18cd4cda09a9b14ceede51
ditowahyu <110106165+ditowahyu@users.noreply.github.com>
2022-08-23 16:42:07 +0700

    task 9

---
+++ dito/bab2/task9/task9.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$database = "perpusdb";
+$username = "root";
+$password = "";
+
+//*create* connection
+$mysqli = mysqli_connect ($servername, $username, $password, $database);
+
+?>
