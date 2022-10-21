4e69abacc4282bd1076fc47f6369b360c82f1f1c
Yongki Surya Alkayyis <yongkisurya99@gmail.com>
2022-10-21 10:20:29 +0700

    create model & migration

--- library/resources/views/home.blade.php
+++ library/resources/views/home.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.admin')
+@section('header', 'Home')
 
 @section('content')
-Ini adalah halaman home
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Ini Adalah Halaman Home</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
