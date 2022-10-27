43dcfb1a9adc23a22581ebacfed03f4c6d2a3d61
FauziNurdiansyah <nurdiansyah.fauzi1@gmail.com>
2022-10-28 00:20:56 +0700

    Eloquent Relation DOne

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
+use App\Models\Book;
+use App\Models\Catalog;
+use App\Models\Member;
+use App\Models\Publisher;
+use App\Models\Transaction;
+use App\Models\TransactionDetail;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +30,27 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $members = Member::with('user')->get();
+        // $books = Book::with('publisher')->get();
+        // $publisher = Publisher::with('books')->get();
+        // $books = Book::with('author')->get();
+        // $author = Author::with('books')->get();
+        // $books = Book::with('catalog')->get();
+        // $catalog = Catalog::with('books')->get();
+        // $books = Book::with('transactiondetails')->get();
+        // $transactiondetails = TransactionDetail::with('book')->get();
+        // $transactiondetails = TransactionDetail::with('transaction')->get();
+        // $transactions = Transaction::with('transactiondetails')->get();
+        // $members = Member::with('transactions')->get();
+        $transactions = Transaction::with('member')->get();
+
+        return $transactions;
+        // return $transactiondetails;
+        // return $catalog;
+        // return $author;
+        // return $publisher;
+        // return $books;
+        return $members;
         return view('home');
     }
 }
--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Transaction extends Model
 {
     use HasFactory;
+
+    public function transactiondetails()
+    {
+        return $this->hasMany('App\Models\TransactionDetail', 'transaction_id');
+    }
+    public function member()
+    {
+        return $this->belongsTo('App\Models\Member', 'member_id');
+    }
 }
