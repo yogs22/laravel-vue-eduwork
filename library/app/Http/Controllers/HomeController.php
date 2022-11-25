be53152ea0b1e64b09a118787fd8b71d75d3d550
Nasrun Habu <nasrunhabu567@gmail.com>
2022-11-26 00:42:45 +0700

    Membuat Relasi di laravel

--- library/app/Http/Controllers/HomeController.php
+++ library/app/Http/Controllers/HomeController.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Author;
+use App\Models\Catalog;
+use App\Models\Publisher;
+use App\Models\Book;
+use App\Models\Member;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +29,19 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $members = Member::with('user')->get();
+        // $books = Book::with('publisher')->get();
+        // $books = Book::with('catalog')->get();
+        // $books = Book::with('author')->get();
+        // $publishers = Publisher::with('books')->get();
+        // $catalogs = Catalog::with('books')->get();
+        // $authors = Author::with('books')->get();
+
+        // return $authors;
+        // return $catalogs;
+        // return $publishers;
+        // return $books;
+        // return $members;
         return view('home');
     }
 }
