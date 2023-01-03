f0208803484485d9eb6cd757fe83d1953b350c52
thisnotme2205 <thisnotme.2205@gmail.com>
2023-01-03 12:37:17 +0700

    route & view

--- library/resources/views/layouts/admin.blade.php
+++ library/resources/views/layouts/admin.blade.php
@@ -89,7 +89,7 @@
   </nav>
 
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <a href="{{ route('home') }}" class="brand-link">
+    <a href="{{ url('home') }}" class="brand-link">
       <img src="{{ asset('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Perpustakaan</span>
     </a>
@@ -119,9 +119,39 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <li class="nav-item">
-            <a href="{{ route('home') }}" class="nav-link active">
+            <a href="{{ url('home') }}" class="nav-link {{ request()->is('home') ? 'active' : '' }}">
               <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>Dashboard</p>
+              <p>Home</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url('catalog') }}" class="nav-link {{ request()->is('catalog') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-list"></i>
+              <p>Catalog</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url('publisher') }}" class="nav-link {{ request()->is('publisher') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-bullhorn"></i>
+              <p>Publisher</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url('author') }}" class="nav-link {{ request()->is('author') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-at"></i>
+              <p>Author</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url('book') }}" class="nav-link {{ request()->is('book') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-book"></i>
+              <p>Book</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="{{ url('member') }}" class="nav-link {{ request()->is('member') ? 'active' : '' }}">
+              <i class="nav-icon fas fa-users"></i>
+              <p>Member</p>
             </a>
           </li>
         </ul>
@@ -134,7 +164,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1 class="m-0">Dashboard</h1>
+            <h1 class="m-0">@yield('header')</h1>
           </div>
         </div>
       </div>
