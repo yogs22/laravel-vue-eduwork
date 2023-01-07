8a9c0679397dcb7fa704472dec8d5b17094e234c
Nasrun Habu <nasrunhabu567@gmail.com>
2023-01-07 20:44:16 +0700

    Membuat Helpers utnuk format Date = Convert_date

--- library/resources/views/admin/catalog/index.blade.php
+++ library/resources/views/admin/catalog/index.blade.php
@@ -38,8 +38,8 @@
 										<td>{{ $keys+1 }}</td>
 										<td>{{$catalog->name}}</td>
 										<td class="text-center">{{count($catalog->books)}}</td>
-										<td class="text-center">{{date('H:i:s - d M Y', strtotime($catalog->created_at))}}</td>
-										<td class="text-center">{{ date('H:i:s - d M Y', strtotime($catalog->updated_at))}}</td>
+										<td class="text-center">{{ convert_date($catalog->created_at) }}</td>
+										<td class="text-center">{{ convert_date($catalog->updated_at) }}</td>
 										<td class="text-right">
 											<a href="{{ route('catalog.edit', $catalog->id) }}" class="btn btn-warning btn-sm">Edit</a>
 										
