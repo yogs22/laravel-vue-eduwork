74f5bad8bf7aa8f5caebe0725718ceaf245f283c
thisnotme2205 <thisnotme.2205@gmail.com>
2023-01-11 23:14:38 +0700

    datatable

--- library/resources/views/admin/publisher/index.blade.php
+++ library/resources/views/admin/publisher/index.blade.php
@@ -2,7 +2,10 @@
 @section('header', 'Publisher')
 
 @section('css')
-  
+  <!-- DataTables -->
+  <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css') }}">
 @endsection
 
 @section('content')
@@ -12,8 +15,8 @@
       <h3 class="card-title">Data Publisher</h3>
       <a @click="addData()" class="btn btn-sm btn-primary float-right">Create New Publisher</a>
     </div>
-    <div class="card-body p-0">
-      <table class="table">
+    <div class="card-body">
+      <table class="table" id="datatable">
         <thead>
           <tr>
             <th style="width: 10px">#</th>
@@ -87,6 +90,25 @@
 @endsection
 
 @section('js')
+  <!-- DataTables  & Plugins -->
+  <script src="{{ asset('assets/plugins/datatables/jquery.dataTables.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/datatables-responsive/js/dataTables.responsive.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/datatables-responsive/js/responsive.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/datatables-buttons/js/dataTables.buttons.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.bootstrap4.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/jszip/jszip.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/pdfmake/pdfmake.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/pdfmake/vfs_fonts.js') }}"></script>
+  <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.html5.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.print.min.js') }}"></script>
+  <script src="{{ asset('assets/plugins/datatables-buttons/js/buttons.colVis.min.js') }}"></script>
+  <script>
+    $(function () {
+      $("#datatable").DataTable();
+    });
+  </script>
+  {{-- function vue --}}
   <script type="text/javascript">
     var controller = new Vue({
       el: '#controller',
